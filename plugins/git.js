00e745ef73aac6a9609ce80a445dcd44b13de606
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-09-22 15:32:40 +0530

    Don't update

---
+++ plugins/git.js
@@ -0,0 +1,29 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('gitlink');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+    r_text[0] = "https://i.imgur.com/2nYs8dN.jpg"
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `creater *Nithin and Fayas*
+\n*owner number wa.me/918137829228*\n\n
+*owner number wa.me/918075379950*\n\n
+*whatsapp group : https://chat.whatsapp.com/BRPbS6JHUoCE480MpLLM5z*\n\n
+*githublink       https://github.com/Chunkindepadayali/LIZA-MWOL*\n\n
+*audio commads    https://github.com/Chunkindepadayali/media/tree/main/uploads*\n\n
+*sticker commads  https://github.com/Chunkindepadayali/media/tree/main/stickers*\n\n
+`}) 
+
+})); 
