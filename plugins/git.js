f456e2a24b60614f144d4cb8167f43c7f71f2b39
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-09-22 14:53:32 +0530

    Don't update 😅

--- plugins/git.js
+++ plugins/git.js
@@ -29,19 +29,12 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `creater *Nithin and Fayas*
-
-*owner number wa.me/918137829228*
-
-*owner number wa.me/918075379950*
-
-*whatsapp group : https://chat.whatsapp.com/BRPbS6JHUoCE480MpLLM5z*
-
-*githublink       https://github.com/Chunkindepadayali/LIZA-MWOL*
-
-*audio commads    https://github.com/Chunkindepadayali/media/tree/main/uploads*
-
-*sticker commads  https://github.com/Chunkindepadayali/media/tree/main/stickers*
-
+\n*owner number wa.me/918137829228*\n\n
+*owner number wa.me/918075379950*\n\n
+*whatsapp group : https://chat.whatsapp.com/BRPbS6JHUoCE480MpLLM5z*\n\n
+*githublink       https://github.com/Chunkindepadayali/LIZA-MWOL*\n\n
+*audio commads    https://github.com/Chunkindepadayali/media/tree/main/uploads*\n\n
+*sticker commads  https://github.com/Chunkindepadayali/media/tree/main/stickers*\n\n
 `}) 
 
 }));
