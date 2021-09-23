2efc73f05b75efb81c106dbce61cf6d2a4ce058b
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-09-23 14:54:02 +0530

    ```If you have not yet joined the whatsapp group, please join. If you have any error issues, please contact us. (Team Lizamwol)```

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -3,7 +3,6 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
-
 const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
