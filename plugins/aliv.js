94e47e5a94bc001b035bf70fb024df80e3cc94ab
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-09-26 20:27:35 +0530

    Don't update

--- plugins/aliv.js
+++ plugins/aliv.js
@@ -61,7 +61,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    QueenSew.newcmdaddtosew({pattern: 'aliv', fromMe: false, desc: Lang.PR}, (async (message, match) => {
+    Asena.addCommand({pattern: 'aliv', fromMe: false, desc: Lang.PR}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -75,7 +75,7 @@ else if (Config.WORKTYPE == 'public') {
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '_*' + Config.OWNERSHIP + '*_\n\n```\nOS       :Mac OS	68k,Power PC ×86_64\nKernel   : 4.4.0- 1093 - aws\nPackage  : 342 (apk)\nSell     : bash 8.0.0\nTerminal : Java Script\nCPU      : AMD Ryzen 9 5900X 12 cores 3.7GHz 105W\nRAM      : Corsair Vengeance LED 16GB Kit (2 x 8GB) 3200MHz\nMemory   : 5120GB / 10240GB (Seagate BarraCuda)\nDownloading : 897mbps\nUploading   : 997mbps\nping        :12```\n\nLIZA-MWOL'})
 
     }));
-    QueenSew.newcmdaddtosew({pattern: 'psysd', fromMe: true, desc: Lang.PR}, (async (message, match) => {
+    Asena.addCommand({pattern: 'psysd', fromMe: true, desc: Lang.PR}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -89,7 +89,7 @@ else if (Config.WORKTYPE == 'public') {
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '_*' + Config.OWNERSHIP + '*_\n\n```\nOS       :Mac OS	68k,Power PC ×86_64\nKernel   : 4.4.0- 1093 - aws\nPackage  : 342 (apk)\nSell     : bash 8.0.0\nTerminal : Java Script\nCPU      : AMD Ryzen 9 5900X 12 cores 3.7GHz 105W\nRAM      : Corsair Vengeance LED 16GB Kit (2 x 8GB) 3200MHz\nMemory   : 5120GB / 10240GB (Seagate BarraCuda)\nDownloading : 897mbps\nUploading   : 997mbps\nping        :12```\n\nLIZA-MWOL'})
 
     }));
-    QueenSew.newcmdaddtosew({pattern: 'sysd', fromMe: false, desc: Lang.PR}, (async (message, match) => {
+    Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.PR}, (async (message, match) => {
 
     var r_text = new Array ();
 
