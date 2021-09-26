df2d37a37307ff9565d1e9e71bebeb89aed435db
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-09-27 02:02:12 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,35 +1,33 @@
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
+const config = require('../config');
 const fs = require("fs")
 const Language = require('../language');
 const Lang = Language.getString('gitlink');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
 
-    var r_text = new Array ();
-    
-    
-    r_text[0] = "https://i.imgur.com/2nYs8dN.jpg"
-    
-    
-    var i = Math.floor(1*Math.random())
+    var rashi = await axios.get(config.RASHI, { responseType: 'arraybuffer' })
 
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.png, caption: `creater *Nithin and Fayas*
-    
+    await message.sendMessage(Buffer(rashi.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
+ 
 *owner number wa.me/918137829228*
-
+   
 *owner number wa.me/918075379950*
+
 
 *whatsapp group : https://chat.whatsapp.com/BRPbS6JHUoCE480MpLLM5z*
 
+
 *githublink       https://github.com/Chunkindepadayali/LIZA-MWOL*
+
 
 *audio commads    https://github.com/Chunkindepadayali/media/tree/main/uploads*
 
-*sticker commads  https://github.com/Chunkindepadayali/media/tree/main/stickers*
+
+*sticker commads  https://github.com/Chunkindepadayali/media/tree/main/stickers*     
 `}) 
 
 })); 
