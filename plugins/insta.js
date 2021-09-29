a7b355b217f586c8ae46e82704a7aa3e4af65ea3
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-09-30 04:07:39 +0530

    Insta download problem fixed Check the update ✔️

--- plugins/insta.js
+++ plugins/insta.js
@@ -10,6 +10,6 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from 
     const { status, type, data } = await instagram(match[1], 'julie')
     if (!status) return await message.sendMessage('not found')
     await message.client.sendMessage(message.jid, LOAD_ING, MessageType.text, { quoted: message.data });
-    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "Made By JulieMwol", quoted: message.data })
-    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "Made By JulieMwol", quoted: message.data })
+    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "```Made By Liza Mwol```", quoted: message.data })
+    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "```Made By Liza Mwol```", quoted: message.data })
 });
