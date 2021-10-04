3b6810c4161461d9e877021873f6924fda471dea
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-10-04 19:45:46 +0530

    Don't update 😐

--- plugins/lizamwolv2.js
+++ plugins/lizamwolv2.js
@@ -18,9 +18,9 @@ var ddd = ggg.toString('utf-8')
 Neotro.addCommand({pattern: 'help', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 // send a list message!
     const rows = [
-        {title: '.ALIVE', description: " ", rowId:" rowid1"},
-        {title: '.MENU', description: "  ", rowId:" rowid2"},
-        {title: '.XMEDIA', description: " ", rowId:" rowid3"},
+        {title: '.ALIVE', description: "", rowId:" rowid1"},
+        {title: '.MENU', description: "", rowId:" rowid2"},
+        {title: '.XMEDIA', description: "", rowId:" rowid3"},
         {title: '.OWNER', description: "\n\n\n```creater Nithin and Fayas```\n\n```owner number wa.me/918137829228```\n\n```owner number wa.me/918075379950```\n\n ", rowId:" rowid4"},
         {title: '.GIT', description: "\n\n\n```creater Nithin and Fayas```\n\n```owner number wa.me/918137829228```\n\n```owner number wa.me/918075379950```\n\n```whatsapp group : https://chat.whatsapp.com/BRPbS6JHUoCE480MpLLM5z```\n\n```githublink       https://github.com/Chunkindepadayali/LIZA-MWOL```\n\n```audio commads    https://github.com/Chunkindepadayali/media/tree/main/uploads```\n\n```sticker commads  https://github.com/Chunkindepadayali/media/tree/main/stickers```\n  ", rowId:" rowid5"}
        ]
