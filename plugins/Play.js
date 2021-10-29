d317805f990c0e3433f66486c21bfa376ba66586
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-10-30 00:36:06 +0530

    Don't update

---
+++ plugins/Play.js
@@ -0,0 +1,26 @@
+const Asena = require('../events');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const got = require('got');
+const LOAD_ING = "*Result found...*"
+const Config = require('../config');
+const axios = require('axios')
+const Axios = require('axios')
+
+
+Asena.addCommand({pattern: 'hi ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
+	
+	await message.client.sendMessage(message.jid, '*wait....*' , MessageType.text, { quoted: message.data });
+	
+	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
+	
+        const { status, result } = data
+	
+	var img = await Axios.get(`${result.thumb}`, {responseType: 'arraybuffer'})
+	
+        if(!status) return await message.sendMessage('*NO RESULT FOUND*')
+	
+        await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
+        let msg = '```'
+        msg +=  `TITLE :${result.title}\n\n`        
+        msg += '```'
+         return awai
