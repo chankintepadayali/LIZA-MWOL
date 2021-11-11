3cb56b5fa8957c8b7d4e31b31933f75d0efb2ac6
Ali <84945779+Aces107@users.noreply.github.com>
2021-11-11 12:23:08 +0300

    Update greetings.js

--- plugins/sql/greetings.js
+++ plugins/sql/greetings.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 Ali ACES.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsAsena - ALI ACES
 */
 
 const config = require('../../config');
