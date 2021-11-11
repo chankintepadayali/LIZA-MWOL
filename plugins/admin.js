2f3a594056082dc1d6698ec7f330f8d7f57b1b05
manibotwa <94069456+manibotwa@users.noreply.github.com>
2021-11-11 08:01:22 +0530

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -90,7 +90,7 @@ Asena.addCommand({pattern: 'add(?: |$)(.*)', fromMe: true, dontAddCommandList: t
 
 Asena.addCommand({pattern: 'promote ?(.*)', fromMe: true, dontAddCommandList: true, onlyGroup: true, desc: Lang.PROMOTE_DESC}, (async (message, match) => {    
     var im = await checkImAdmin(message);
-    if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
+    if (!im) return await message.client.sendMessage(message.jid,Lang.😹IM_NOT_ADMIN,MessageType.text);
 
     if (Config.PROMOTEMSG == 'default') {
         if (message.reply_message !== false) {
