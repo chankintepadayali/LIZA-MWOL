f81b2ac5e3df630eec7e27951fe8586ce30f2725
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-11-12 09:07:53 +0530

    `Song new adedd `

--- plugins/song.js
+++ plugins/song.js
@@ -1,7 +1,7 @@
 const Asena = require('../events');
 const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 const got = require('got');
-const LOAD_ING = "```wait cheyy```"
+const LOAD_ING = "```Result found...```"
 const Config = require('../config');
 const axios = require('axios')
 const Axios = require('axios')
@@ -9,7 +9,7 @@ const Axios = require('axios')
 
 Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
 	
-	await message.client.sendMessage(message.jid, '```hey dude```' , MessageType.text, { quoted: message.data });
+	await message.client.sendMessage(message.jid, '```hey dude wait cheyy```' , MessageType.text, { quoted: message.data });
 	
 	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
 	
@@ -17,7 +17,7 @@ Asena.addCommand({pattern: 'song ?(.*)', fromMe: false, desc: 'play song' , dont
 	
 	var img = await Axios.get(`${result.thumb}`, {responseType: 'arraybuffer'})
 	
-        if(!status) return await message.sendMessage('*NO RESULT FOUND*')
+        if(!status) return await message.sendMessage('```NO RESULT FOUND```')
 	
         await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
         let msg = '```'
