e3ae4cd3aa79061eddaffb5e674bd0eb089c73bf
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2021-11-14 06:50:03 +0530

    Fixing bug..

--- config.js
+++ config.js
@@ -64,7 +64,7 @@ module.exports = {
     LIZA: process.env.LOGO_LINK === undefined ? 'https://i.imgur.com/2nYs8dN.jpg' : process.env.LOGO_LINK,
     MWOL: process.env.BGM_DURATION === undefined ? '39999600' : process.env.BGM_DURATION,
     BOTV2: process.env.BOTV2_NAME === undefined ? 'ꪶ͢ɪͥᴛͭsᷤ ͢ᴍͫᴇͤᡃ⃝ʟɪᴢᴀ ᴍᴏʟ࿐' : process.env.BOTV2_NAME,
-    LIZA_AI: process.env.ZARA_AI === undefined ? 'false' : process.env.LIZA_AI,
+    LIZA_AI: process.env.LIZA_AI === undefined ? 'false' : process.env.LIZA_AI,
     BRANCH: 'master',
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
