08c521fab7576e722b9a3f946d7b85eb0ce9d35d
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2022-01-12 18:21:18 +0530

    `Liza Mwol V2 update available`

--- plugins/clear.js
+++ plugins/clear.js
@@ -1,6 +1,6 @@
 /* Copyright (C) 2020 afnanplk.
 Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
+you may not use this file except in compliance with the License. 
 */
 
 
