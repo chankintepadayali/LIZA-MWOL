e7712ce6b6524c6a36eac8e0724640b509ab9901
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2022-01-12 18:14:58 +0530

    `Liza Mwol V2 update available`

--- plugins/afk.js
+++ plugins/afk.js
@@ -4,7 +4,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
-Developer & Co-Founder - Phaticusthiccy
+Developer & Co-Founder - Phaticusthiccy 
 */
 
 const Asena = require('../events');
