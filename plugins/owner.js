a2a3a3de0af5e5fcdf6b021ac3d1237a876a44f6
Arjunser <92748603+Arjunser5@users.noreply.github.com>
2022-01-17 19:43:24 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*LIZA-MWOL by Fayas and Nithin*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*ARJUN-SER by ARJUN LAL OR ARJUNZ🪡*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*LIZA-MWOL by Fayas and Nithin*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*ARJUN-SER by ARJUN LAL OR ARJUNZ🪡*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
