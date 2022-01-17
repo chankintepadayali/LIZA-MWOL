df106efe44575f19503d9aa300c895462b02618b
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2022-01-17 19:55:58 +0530

    `Liza Mwol V2 update available`

--- config.js
+++ config.js
@@ -69,6 +69,7 @@ module.exports = {
     ALIVE_BUTTON: process.env.ALIVE_BUTTON === undefined ? 'ʜɪ ᴅᴜᴅᴇ' : process.env.ALIVE_BUTTON,
     LG_LOGO: process.env.LG_LOGO === undefined ? 'https://telegra.ph/file/0e0409148fa47cdb92582.jpg' : process.env.LG_LOGO,
     LOGO_NAME: process.env.LOGO_NAME === undefined ? 'ꪶ͢ɪͥᴛͭsᷤ ͢ᴍͫᴇͤᡃ⃝ʟɪᴢᴀ ᴍᴏʟ࿐' : process.env.LOGO_NAME,
+    FAKER: process.env.ANTI_FAKE_REMOVE === undefined ? 'false' : process.env.ANTI_FAKE_REMOVE,
     BRANCH: 'master',
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
