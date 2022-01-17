387af1016738f175d188bf9aa194692f39b2098d
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2022-01-18 01:17:05 +0530

    `Liza Mwol V2 update available`

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2020 Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-WhatsAsena - Yusuf Usta
+WhatsAsena - Yusuf Usta 
 */
 
 const fs = require("fs");
