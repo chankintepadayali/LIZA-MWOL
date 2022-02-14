04ccf087853f5052556aa291b9b9a841cc8fcf4d
(hunk!nd3 p4d4y41! <68646925+Chunkindepadayali@users.noreply.github.com>
2022-02-14 17:45:06 +0530

    Tying

--- plugins/gitv2.js
+++ plugins/gitv2.js
@@ -12,7 +12,7 @@ const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'FN:LIZA MWOL [OWNER]\n' // full name
             + 'ORG:ʟɪᴢᴀ ᴍᴡᴏʟ™;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=918075379950:+91 8137829228\n' // WhatsApp ID + phone number
-            + 'TEL;type=CELL;type=VOICE;waid=918137829228:+91 8075379950\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid= Config.NU: Config.NU\n' // 
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "LIZA MWOL [OWNER]", vcard: vcard}, MessageType.contact, { mimetype: Mimetype.contact, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('liza mwol v2.jpg'), surface: 200, message: Config.BOTV2, orderTitle: Config.BOTV2, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOTV2 + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('liza mwol v2.jpg')}}}});
 }))
